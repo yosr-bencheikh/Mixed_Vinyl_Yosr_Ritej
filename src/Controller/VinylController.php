7bcb1ce2a7fb3c1182af56340401ff667396b206
Yosr Ben Cheikh Brehim <yosrbencheikh28@gmail.com>
2024-02-26 15:14:03 +0100

    Added some Tiwggy goodness

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,16 +2,29 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Symfony\Component\Routing\Attribute\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage()
     {
-        return new Response('DSI 22');
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'DSI 22',
+            'tracks' => $tracks,
+        ]);
     }
     #[Route('/browse/{slug}')]
     public function browse(string $slug = null): Response
